fc76bda6e43798893f547233af5ae34881e3fb7c
ericchase <ericnalley@gmail.com>
2017-08-07 01:40:01 -0500

    updated code: github.com-logo--links-to-profile

--- github.com-logo--links-to-profile/code/github.com-logo--links-to-profile.user.js
+++ github.com-logo--links-to-profile/code/github.com-logo--links-to-profile.user.js
@@ -8,7 +8,6 @@
 // @icon
 // @icon64
 // @updateURL
-// @downloadURL     https://greasyfork.org/en/scripts/32087-github-com-logo-links-to-profile
 // @supportURL      https://github.com/ericchase/userscripts/issues
 // @include         https://github.com/*
 // @match
@@ -34,49 +33,62 @@ var observer;
 // observer is hooked, the timer is removed, and the observer handles any
 // necessary updates from then on.
 (function () {
-     'use strict';
-     var handle = setInterval(
-          function () {
-               // The 'div' element with id 'directory-list' contains all the
-               // stream preview objects for each tab of the 'following' page.
-               target = document.querySelector("body > div.position-relative.js-header-wrapper > div.header > div > div");
-               if (target !== null) {
-                    clearInterval(handle);
-                    // The 'subtree' option is necessary to observe the element
-                    // mentioned above. Without it, the observer will never
-                    // trigger.
-                    config = {
-                         attributes: true,
-                         childList: true,
-                         characterData: true,
-                         subtree: true
-                    };
-                    observer = new MutationObserver(update);
-                    observer.observe(target, config);
-                    update(null);
-               }
-          },
-          50
-     );
+    'use strict';
+    var handle = setInterval(
+        function () {
+            // The 'div' element with id 'directory-list' contains all the
+            // stream preview objects for each tab of the 'following' page.
+            target = document.querySelector("body > div.position-relative.js-header-wrapper > div.header > div > div");
+            if (target !== null) {
+                clearInterval(handle);
+                // The 'subtree' option is necessary to observe the element
+                // mentioned above. Without it, the observer will never
+                // trigger.
+                config = {
+                    attributes: true,
+                    childList: true,
+                    characterData: true,
+                    subtree: true
+                };
+                observer = new MutationObserver(update);
+                observer.observe(target, config);
+                update(null);
+            }
+        },
+        50
+    );
 })();
 
 
 function update(mutations) {
 
-     observer.disconnect();
+    observer.disconnect();
 
-     if (document.querySelector("body > div.position-relative.js-header-wrapper > div.header > div > div > div:nth-child(1) > a") === null) {
-          observer.observe(target, config);
-          return;
-     }
 
-     if (document.querySelector("#user-links > li:nth-child(3) > a") === null) {
-          observer.observe(target, config);
-          return;
-     }
+    // check to see if logo has loaded
+    if (document.querySelector("body > div.position-relative.js-header-wrapper > div.header > div > div > div:nth-child(1) > a") === null) {
+        observer.observe(target, config);
+        return;
+    }
 
-     document.querySelector("body > div.position-relative.js-header-wrapper > div.header > div > div > div:nth-child(1) > a").setAttribute(
-          "href",
-          document.querySelector("#user-links > li:nth-child(3) > a").getAttribute("href")
-     );
+
+    // check to see if profile button has loaded
+    if (document.querySelector("#user-links > li:nth-child(3) > a") === null) {
+        observer.observe(target, config);
+        return;
+    }
+
+
+    var url_profile = document.querySelector("#user-links > li:nth-child(3) > a").getAttribute("href");
+
+    // if current page is user's profile, then do nothing
+    if (url_profile == window.location.pathname) {
+        return;
+    }
+
+    var logo = document.querySelector("body > div.position-relative.js-header-wrapper > div.header > div > div > div:nth-child(1) > a");
+
+    // change logo's link
+    logo.setAttribute("href", url_profile);
+
 }
