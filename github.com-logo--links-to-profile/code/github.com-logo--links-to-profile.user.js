626fdd27b0df9f92f78adf00b55ce2754de62a00
ericchase <ericnalley@gmail.com>
2017-08-09 05:00:07 -0500

    updated code: twitch & github

--- github.com-logo--links-to-profile/code/github.com-logo--links-to-profile.user.js
+++ github.com-logo--links-to-profile/code/github.com-logo--links-to-profile.user.js
@@ -26,27 +26,27 @@
 var target;
 var config;
 
-
 // The setInterval() function with a high frequency is used to hook a
 // MutationObserver as quickly as possible to the necessary element. Once the
 // observer is hooked, the timer is removed, and the observer handles any
 // necessary updates from then on.
-(function () {
+(function ()
+{
     'use strict';
     var handle = setInterval(
-        function () {
+        function ()
+        {
             // The 'div' element with id 'directory-list' contains all the
             // stream preview objects for each tab of the 'following' page.
             target = document.querySelector("body > div.position-relative.js-header-wrapper > div.header > div > div");
-            if (target !== null) {
+            if (target !== null)
+            {
                 clearInterval(handle);
                 // The 'subtree' option is necessary to observe the element
                 // mentioned above. Without it, the observer will never
                 // trigger.
                 config = {
-                    attributes: true,
                     childList: true,
-                    characterData: true,
                     subtree: true
                 };
                 var observer = new MutationObserver(update);
@@ -59,32 +59,35 @@ var config;
 })();
 
 
-function update(mutations, observer) {
+function update(mutations, observer)
+{
 
     // check to see if logo has loaded
-    if (document.querySelector("body > div.position-relative.js-header-wrapper > div.header > div > div > div:nth-child(1) > a") === null) {
+    if (document.querySelector("body > div.position-relative.js-header-wrapper > div.header > div > div > div:nth-child(1) > a") === null)
+    {
         return;
     }
 
 
     // check to see if profile button has loaded
-    if (document.querySelector("#user-links > li:nth-child(3) > a") === null) {
+    if (document.querySelector("#user-links > li:nth-child(3) > a") === null)
+    {
         return;
     }
 
-
-    var url_profile = document.querySelector("#user-links > li:nth-child(3) > a").getAttribute("href");
 
     // if current page is user's profile, then do nothing
-    if (url_profile == window.location.pathname) {
+    var url_profile = document.querySelector("#user-links > li:nth-child(3) > a").getAttribute("href");
+    if (url_profile == window.location.pathname)
+    {
         return;
     }
 
 
-    var logo = document.querySelector("body > div.position-relative.js-header-wrapper > div.header > div > div > div:nth-child(1) > a");
-
     // change the logo's link
-    if (logo.getAttribute("href") != url_profile) {
+    var logo = document.querySelector("body > div.position-relative.js-header-wrapper > div.header > div > div > div:nth-child(1) > a");
+    if (logo.getAttribute("href") != url_profile)
+    {
         observer.disconnect();
         logo.setAttribute("href", url_profile);
     }
