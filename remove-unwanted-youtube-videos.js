9592c8eef18060c483a16b5232f6324b29383c98
ericchase <>
2022-05-28 03:38:33 -0500

    feat: rewrite of video processing code

--- remove-unwanted-youtube-videos.js
+++ remove-unwanted-youtube-videos.js
@@ -1,28 +1,117 @@
 // ==UserScript==
-// @name        Remove Unwanted Videos
+// @name        Remove YouTube Videos
 // @namespace   Violentmonkey Scripts
 // @match       https://www.youtube.com/
 // @grant       none
 // @version     1.0
-// @author      ericchase
+// @author      -
 // @description 5/22/2022, 10:37:37 PM
 // ==/UserScript==
 
+// some of this code has been adapted from a similar project by a
+// developer known as bubba (aka skaizo)
+
+
+const config = {
+    maxAge: parseAge('1 year'),
+    // minRuntime: toSeconds('9:00'),
+    // maxRuntime: toSeconds('2:30:00'),
+
+    dismissIfLive: true,
+    dismissIfRemix: true,
+    dismissIfWatched: true,
+};
+
 const text_to_remove = [
-    'china',
-    'acoustic',
-    'music',
+    'Acoustic',
+    'Beats',
+    'China',
+    'Covers',
+    'Crypto',
+    'Hits',
+    'K.A.R.D',
+    'KARD',
+    'Minecraft',
+    'Mix',
+    'Music',
+    'Prank',
+    'Songs',
+    'TikTok',
     'years ago'
 ].map(text => text.toLowerCase());
 
 function shouldBeRemoved(video) {
+    const selectors = {
+        isRemix: 'yt-icon.ytd-thumbnail-overlay-bottom-panel-renderer',
+        isLive: '.badge-style-type-live-now, .badge-style-type-live-now-alternate',
+        videoProgress: 'ytd-thumbnail-overlay-resume-playback-renderer',
+    };
+
+    const videoAge = parseVideoAge(video);
+    if (videoAge > config.maxAge) return true;
+
+    // const videoRuntime = parseVideoRuntime(video);
+    // if (videoRuntime < config.minRuntime) return true;
+    // if (videoRuntime > config.maxRuntime) return true;
+
+    if (config.dismissIfLive && video.querySelector(selectors.isLive)) return true;
+    if (config.dismissIfRemix && video.querySelector(selectors.isRemix)) return true;
+    if (config.dismissIfWatched && video.querySelector(selectors.videoProgress)) return true;
+
     const innerText = video.innerText.toLowerCase();
     for (const text of text_to_remove) {
         if (innerText.includes(text)) {
             return true;
         }
     }
+
     return false;
+}
+
+function parseVideoAge(video) {
+    try {
+        const el = video.querySelector('[id="metadata-line"] span:nth-child(2)');
+        return parseAge(el.innerHTML);
+    } catch {
+        return 0;
+    }
+}
+
+function parseAge(ageString) {
+    if (!ageString) return 0;
+
+    ageString = ageString.split(' ', 2);
+
+    const unit = ageString[1];
+    const count = parseInt(ageString[0]);
+
+    if (unit.includes('day')) return count;
+    if (unit.includes('week')) return count * 7;
+    if (unit.includes('year')) return count * 365;
+    if (unit.includes('month')) return count * 30;
+
+    return 0;
+}
+
+function parseVideoRuntime(video) {
+    try {
+        const el = video.querySelector('ytd-thumbnail-overlay-time-status-renderer span');
+        return toSeconds(el.innerHTML);
+    } catch {
+        return config.minRuntime;
+    }
+}
+
+function toSeconds(timeString) {
+    if (!timeString) return;
+
+    timeString = timeString.split(':');
+    const timePieces = timeString.length;
+
+    if (timePieces < 2 || timePieces > 3) return;
+    if (timePieces === 2) return parseInt(timeString[0] * 60) + parseInt(timeString[1]);
+
+    return parseInt(timeString[0] * 3600) + parseInt(timeString[1] * 60) + parseInt(timeString[2]);
 }
 
 function hideVideo(video) {
@@ -32,15 +121,10 @@ function hideVideo(video) {
     } catch { }
 }
 
-function openMenu(video) {
+function getMenu(video) {
     return new Promise((resolve, reject) => {
         const dots = video.querySelector('yt-icon-button yt-icon');
-        if (!dots)
-            reject();
-        else {
-            dots.click();
-            resolve(dots);
-        }
+        if (!dots) resolve(null); else resolve(dots);
     });
 }
 
@@ -56,35 +140,56 @@ function clickMenuItem(menuItems, itemName) {
 
 const found_videos = new Set();
 const unprocessed_videos = [];
-
 let unprocessed_index = 0;
 
-async function processVideo() {
+function isVisible(el) {
+    return window.getComputedStyle(el).display !== 'none';
+}
+
+async function checkForMenu() {
     if (unprocessed_index < unprocessed_videos.length) {
-        // console.log(`processing videos... ${unprocessed_videos.length - unprocessed_index} left`);
+        const video = unprocessed_videos[unprocessed_index];
+        if (video && isVisible(video)) {
+            const dots = await getMenu(video);
+            if (dots) {
+                unprocessed_index = 0;
+                setTimeout(() => processVideos(), 50);
+                return;
+            }
+        }
+        ++unprocessed_index;
+    } else {
+        unprocessed_index = 0;
+    }
+    setTimeout(() => checkForMenu(), 50);
+}
+
+function sleep(ms) { return new Promise(resolve => setTimeout(resolve, ms)); }
+
+async function processVideos() {
+    if (unprocessed_index < unprocessed_videos.length) {
         const video = unprocessed_videos[unprocessed_index];
 
-        if (shouldBeRemoved(video)) {
+        if (video && isVisible(video) && shouldBeRemoved(video)) {
 
-            // Just hide the video
-            hideVideo(video);
-            ++unprocessed_index;
-
-            // Click the 'Not Interested' menu item
-            // try {
-            //     const dots = await openMenu(video);
-            //     const menuItems = document.querySelectorAll('ytd-menu-popup-renderer ytd-menu-service-item-renderer');
-            //     if (!clickMenuItem(menuItems, "Not interested"))
-            //         dots.click(); // close menu
-            //     ++unprocessed_index;
-            // }
-            // catch (e) { };
-
-        } else {
-            ++unprocessed_index;
+            // hideVideo(video);
+            const dots = await getMenu(video);
+            if (dots) {
+                const scrollY = window.scrollY;
+                dots.click();
+                await sleep(10);
+                const menuItems = document.querySelectorAll('ytd-menu-popup-renderer ytd-menu-service-item-renderer');
+                if (!clickMenuItem(menuItems, "Not interested")) {
+                    dots.click(); // close menu
+                }
+                await sleep(10);
+                window.scrollTo(0, scrollY);
+            }
         }
+
+        ++unprocessed_index;
     }
-    setTimeout(() => processVideo(), 100);
+    setTimeout(() => processVideos(), 50);
 }
 
 function watchForVideos(container) {
@@ -98,7 +203,7 @@ function watchForVideos(container) {
             }
         }
     }).observe(container, { subtree: true, childList: true });
-    setTimeout(() => processVideo(), 100);
+    setTimeout(() => checkForMenu(), 50);
 }
 
 new MutationObserver(function (_, observer) {
