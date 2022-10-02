33e05a3a0712fd531d3e929c14d5394dc6b69964
ericchase <github@eric.addymail.com>
2022-10-02 06:21:28 -0500

    fix: handle both modal and non-modal videos
    Signed-off-by: ericchase <github@eric.addymail.com>

--- instagram--remove-image-video-overlay-on-right-click.user.js
+++ instagram--remove-image-video-overlay-on-right-click.user.js
@@ -9,20 +9,61 @@
 // @run-at      document-start
 // ==/UserScript==
 
-document.body.addEventListener('mousedown', function (e) {
-    if (e.button === 2) {
-        if (e.target.parentElement.querySelector(':scope>div>img')) {
-            e.target.remove();
-            return;
-        }
+function grabImageElement(el) {
+    const elImage = el.querySelector(':scope>div>img');
+    return { elImage };
+}
 
-        const videoElement = e.target.parentElement.querySelector(':scope>div>div>div>video');
-        if (videoElement) {
-            videoElement.setAttribute('controls', true);
-            e.target.nextElementSibling?.remove();
-            e.target.previousElementSibling?.remove();
-            e.target.remove();
-            return;
+function grabVideoElement(el) {
+    // The modal video is displayed in the sort of focused component that is
+    // overlayed on top of the page. The page in the background is then
+    // slightly obscurred with a transparent black mask.
+    const elModalVideo = el.parentElement.querySelector(':scope>div>div>div>video');
+    const elVideo = el.parentElement.parentElement.parentElement.querySelector(':scope>video');
+    return { elModalVideo, elVideo };
+}
+
+function removeChildren(elParent, elIgnore) {
+    const elRemovalList = [];
+    for (const elChild of elParent.children)
+        if (elChild !== elIgnore && !elChild.contains(elIgnore))
+            elRemovalList.push(elChild);
+    for (const el of elRemovalList)
+        el.remove();
+}
+
+function removeOveralElement(mouseEvent, elTarget, elParent) {
+    if (mouseEvent.button === 2) {
+        const { elImage } = grabImageElement(elParent);
+        if (elImage) {
+            elTarget.remove();
+            return true;
         }
+        const { elModalVideo, elVideo } = grabVideoElement(elTarget);
+        if (elModalVideo) {
+            elModalVideo.setAttribute('controls', true);
+            removeChildren(elParent, elModalVideo);
+            return true;
+        } else if (elVideo) {
+            elVideo.setAttribute('controls', true);
+            elParent.remove();
+            return true;
+        }
+        console.log('No overlay found?');
+        return false;
+    }
+}
+
+function preventContextMenuPopupOnce(e) {
+    window.removeEventListener('contextmenu', preventContextMenuPopupOnce);
+    e.preventDefault();
+}
+
+document.body.addEventListener('mousedown', function (e) {
+    if (removeOveralElement(e, e.target, e.target.parentElement)) {
+        window.addEventListener('contextmenu', preventContextMenuPopupOnce);
+        e.preventDefault();
+        e.stopImmediatePropagation();
+        e.stopPropagation();
     }
 });
