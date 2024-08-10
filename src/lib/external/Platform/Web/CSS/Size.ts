38c1dd85086d81f459a73548d70b10b5b00cadbd
ericchase <github@eric.addymail.com>
2024-08-10 08:58:56 -0500

    update
    Signed-off-by: ericchase <github@eric.addymail.com>

--- src/lib/external/Platform/Web/CSS/Size.ts
+++ src/lib/external/Platform/Web/CSS/Size.ts
@@ -3,4 +3,12 @@ export function ToRelativePx(px: number, root: HTMLElement | SVGElement = docume
   return (fontSizePx / 16) * px;
 }
 
-// function ToRelativeEM() {} ??
+export function ToAdjustedEm(em: number, root: HTMLElement | SVGElement = document.documentElement) {
+  const fontSizePx = Number.parseInt(getComputedStyle(root).fontSize);
+  return (16 / fontSizePx) * em;
+}
+
+export function ToRelativeEm(em: number, root: HTMLElement | SVGElement = document.documentElement) {
+  const fontSizePx = Number.parseInt(getComputedStyle(root).fontSize);
+  return (fontSizePx / 16) * em;
+}
